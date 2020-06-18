a9330bd15472f14cf6c72240283799fbd8dfe845
tonightyamada99 <tonightyamada99@gmail.com>
2020-06-18 19:02:30 +0900

    ガチエリアのカウントを文字認識で取得してみた。うまくいかなかった。

--- ikaChar.py
+++ ikaChar.py
@@ -2,81 +2,125 @@
 
 import cv2
 import csv
-import os.path
-import numpy as np
-
-
-
-
-# ocr_card.py
 import os
+import os.path
 from PIL import Image
 import pyocr
 import pyocr.builders
 
-# 1.インストール済みのTesseractのパスを通す
+
+TL_time_ratio = (0.475, 0.05)      # at FHD ( 912, 54)
+BR_time_ratio = (0.525, 0.091666)  # at FHD (1008, 99)                   
+
+TL_count_ratio = [(804/1920, 154/1080), (1026/1920, 154/1080)]  # at FHD 1920*1080
+BR_count_ratio = [(894/1920, 210/1080), (1116/1920, 210/1080)]
+
+
+# HSVの閾値
+# 黄
+hsv_y_min = (90, 0, 128)
+hsv_y_max = (100, 255, 255)
+# 青
+hsv_b_min = (165, 128, 128)
+hsv_b_max = (175, 255, 255)
+# 白
+hsv_w_min = (0, 0, 0)
+hsv_w_max = (179, 128, 255)
+# 黄と青と白
+hsv_all_min = [hsv_y_min, hsv_b_min, hsv_w_min]
+hsv_all_max = [hsv_y_max, hsv_b_max, hsv_w_max]
+
+
+# GRAYの閾値
+threshold_list = [80, 160, 224] #224
+
+
+# 以下OCR関連
+# インストール済みのTesseractのパスを通す
 path_tesseract = "C:\\Program Files\\Tesseract-OCR"
 if path_tesseract not in os.environ["PATH"].split(os.pathsep):
     os.environ["PATH"] += os.pathsep + path_tesseract
 
-# 2.OCRエンジンの取得
+# OCRエンジンの取得
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
-
-
-TL_time_ratio = (0.475, 0.05)      # at FHD ( 912, 54)
-BR_time_ratio = (0.525, 0.091666)  # at FHD (1008, 99)
-                   
+# OCR設定
+builder = pyocr.builders.DigitBuilder(tesseract_layout=4)   # layoutは調整が必要かも  
+lang = 'eng+jpn'
 
 
 
-# TopLeft, BottomRight
-TLBR_y = [(480, 40), (920, 100)] 
-TLBR_b = [(1000, 40), (1440, 100)]
-
-TLBR_list = [TLBR_y, TLBR_b]
-
-# HSVの閾値
-hsv_y_min = (90, 128, 128)
-hsv_y_max = (100, 255, 255)
-
-hsv_b_min = (165, 128, 128)
-hsv_b_max = (175, 255, 255)
-
-hsv_w_min = (0, 0, 224)
-hsv_w_max = (179, 10, 255)
-
-hsv_a_min = [hsv_y_min, hsv_b_min]
-hsv_a_max = [hsv_y_max, hsv_b_max]
-
-
-# 各プレイヤーのイカランプの位置
-# ピンチ表示に対して調整が必要
-TL_lamp = [(550, 42), (642, 42), (715, 42), (792, 42), (1044, 42), (1130, 42), (1227, 42), (1323, 42)] 
-BR_lamp = [(596, 98), (692, 98), (789, 98), (875, 98), (1127, 98), (1204, 98), (1277, 98), (1369, 98)]
-
-
-def cv2pil(image_cv):
-    image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
-    image_pil = Image.fromarray(image_cv)
-    image_pil = image_pil.convert('RGB')
-
-    return image_pil
-
+def cv2pil(image):
+    ''' OpenCV型 -> PIL型 '''
+    new_image = image.copy()
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
+        
+    new_image = Image.fromarray(new_image)
     
-def jugChar(video_path, frame_start, frame_end):
-    ''' イカランプの判別 '''      
+    return new_image
+
+
+def zonesCount(frame, method):
+    H, W = frame.shape[:2]
+
+    char_list = []
+    for i in [0, 1]:
+        TL_count = (round(W * TL_count_ratio[i][0]), round(H * TL_count_ratio[i][1]))
+        BR_count = (round(W * BR_count_ratio[i][0]), round(H * BR_count_ratio[i][1]))
+        
+        img_trimmed = frame[TL_count[1] : BR_count[1], TL_count[0] : BR_count[0]]
+        
+        if method == 'HSV':
+            # RGB -> BGR -> HSV
+            img_bgr = cv2.cvtColor(img_trimmed, cv2.COLOR_RGB2BGR)
+            img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+            
+            for j in range(len(hsv_all_min)):
+                # HSV閾値で2値化
+                img_bin = cv2.inRange(img_hsv, hsv_all_min[j], hsv_all_max[j])     
+                # OpenCV型 -> PIL型
+                img_tgt = cv2pil(img_bin)
+                # img_tgt.show()
+                # OCRで文字認識
+                result = tool.image_to_string(img_tgt, lang=lang, builder=builder)   
+                char_list.append(result)  
+                
+        elif method == 'GRAY':
+            # グレースケール
+            img_gray = cv2.cvtColor(img_trimmed, cv2.COLOR_RGB2GRAY)
+            
+            for j in range(len(threshold_list)):
+                # グレースケール
+                ret, img_bin = cv2.threshold(img_gray, threshold_list[j], 255, cv2.THRESH_BINARY_INV)
+                # OpenCV型 -> PIL型
+                img_tgt = cv2pil(img_bin)
+                # img_tgt.show()
+                # OCRで文字認識
+                result = tool.image_to_string(img_tgt, lang=lang, builder=builder)   
+                char_list.append(result)      
+            
+        else:
+            print('Enter "method" in HSV or GRAY')
+            break
+    
+    return char_list
+
+
+
+
+def recogChar(video_path, frame_start, frame_end):
+    ''' 動画で文字認識 '''      
     video_name, video_ext = os.path.splitext(os.path.basename(video_path))
     
     video = cv2.VideoCapture(video_path)
-    W = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-    H = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    TL_time = (round(W * TL_time_ratio[0]), round(H * TL_time_ratio[1]))
-    BR_time = (round(W * BR_time_ratio[0]), round(H * BR_time_ratio[1]))
-
-    time_list = [] # record
+    count_list = [['fcount', 'yellow80', 'yellow160', 'yellow224', 'blue80', 'blue160', 'blue224']] # record
 
     fcount = 0
     while(video.isOpened()):
@@ -88,66 +132,52 @@ def jugChar(video_path, frame_start, frame_end):
         fcount = fcount + 1  
         
         if frame_start <= fcount < frame_end:
-            img_trimmed = frame[TL_time[1] : BR_time[1], TL_time[0] : BR_time[0]]
-        
-            img_bgr = cv2.cvtColor(img_trimmed, cv2.COLOR_RGB2BGR)
-            img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)        
-            img_bin = cv2.inRange(img_hsv, hsv_w_min, hsv_w_max)
-            
-            img_org = cv2pil(img_bin)
-        
-            builder = pyocr.builders.TextBuilder()
-            lang = 'eng'
-            result = tool.image_to_string(img_org, lang=lang, builder=builder)
-            
-   
-            time_list.append([result])
+            if (fcount - frame_start) % 15 == 0:
+                # この中に行いたい処理を書く
+                count = zonesCount(frame, 'GRAY')
+       
+                count_list.append([fcount] + count)
                 
         elif fcount == frame_end:
             break
                                 
     video.release
-
+    
     # CSV出力
-    with open(video_name + '_time.csv', 'w') as file:
+    with open(video_name + '_count.csv', 'w') as file:
         writer = csv.writer(file, lineterminator='\n')
-        writer.writerows(time_list)
+        writer.writerows(count_list)
         
-
-def jugLampImg(img_path): 
-    ''' 画像で文字判別の練習 '''
-    frame = cv2.imread(img_path)
-    height_img, width_img, channels = frame.shape[:3]
-
-    TL_time = (round(width_img * TL_time_ratio[0]), round(height_img * TL_time_ratio[1]))
-    BR_time = (round(width_img * BR_time_ratio[0]), round(height_img * BR_time_ratio[1]))
+        
+def binalization(image):
+    ''' 2値化のチェック '''
+    for i in range(16):
+        threshold = int(i*16)
     
-    img_trimmed = frame[TL_time[1] : BR_time[1], TL_time[0] : BR_time[0]]
-
-    img_bgr = cv2.cvtColor(img_trimmed, cv2.COLOR_RGB2BGR)
-    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)        
-    img_bin = cv2.inRange(img_hsv, hsv_w_min, hsv_w_max)
+        img_gry = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        ret, img_bin = cv2.threshold(img_gry, threshold, 255, cv2.THRESH_BINARY_INV)
+        
+        cv2.imshow('threshold = ' + str(threshold), img_bin)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     
-    img_org = cv2pil(img_bin)
-
-    # 4.ＯＣＲ実行
-    builder = pyocr.builders.TextBuilder()
-    lang = 'jpn+eng'
-    result = tool.image_to_string(img_org, lang=lang, builder=builder)
-    
-    print(result)    
-
-                    
     
 def main():
     ''' メイン処理 ''' 
-    # img_path = '.\\testimg_1.png'
-    # jugLampImg(img_path)
+    img_path = 'image.png'
+    frame = cv2.imread(img_path)
     
-    video_path = 'D:\\splatoon_movie\\capture\\overlook_turf\\overlook_turf_stage0.avi'
-    frame_start = 484
-    frame_end = 1000
-    jugChar(video_path, frame_start, frame_end)
+    # for method in ['HSV', 'GRAY']:
+    #     count = zonesCount(frame, method) 
+    #     print(count)
+    
+    # binalization(frame) 
+    
+    
+    video_path = 'video.mp4'
+    frame_start = 1
+    frame_end = 9999
+    recogChar(video_path, frame_start, frame_end)
     
     
         
