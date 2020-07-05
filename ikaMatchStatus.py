1f068fdfa7cb53c97d4864292970993e8edd542e
tonightyamada99 <tonightyamada99@gmail.com>
2020-07-05 20:34:09 +0900

    zones_numを記録するように変更

--- ikaMatchStatus.py
+++ ikaMatchStatus.py
@@ -6,10 +6,11 @@ Created on Wed Apr  1 19:27:47 2020
 """
 import cv2
 import csv
+import csvread
 import os.path
+import glob
 
 
-''' Screen Coodinate Settings '''
 
 # 時計の座標, FINISH!の座標
 TL_time_ratio = [( 912/1920, 54/1080), (380/1920, 630/1080)]
@@ -26,178 +27,190 @@ BR_stage_ratio = [(1890/1920, 1053/1080)]
 
 # ルール一覧
 rule_list = ['turf', 'zones', 'tower', 'rain', 'clam']
-
+# 一致率の閾値
 threshold = 0.9
 
-
+stage_list = csvread.csvread('stage.csv', 's')
 
 
 def getStatus(video_path, out_path):
     ''' 動画の情報を調べる '''    
-    video = cv2.VideoCapture(video_path)   
-    H = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    W = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-    fps = video.get(cv2.CAP_PROP_FPS)
-    frame_all = video.get(cv2.CAP_PROP_FRAME_COUNT)
-    
-    # 動画の画面サイズに応じて切り取り範囲を設定
-    TL_rule = []
-    BR_rule = []
-    for i in range(len(TL_rule_ratio)):
-        TL = (round(W * TL_rule_ratio[i][0]), round(H * TL_rule_ratio[i][1]))
-        BR = (round(W * BR_rule_ratio[i][0]), round(H * BR_rule_ratio[i][1])) 
-        TL_rule.append(TL)
-        BR_rule.append(BR)
 
-    TL_time = []
-    BR_time = []
-    for i in range(len(TL_time_ratio)):
-        TL = (round(W * TL_time_ratio[i][0]), round(H * TL_time_ratio[i][1]))
-        BR = (round(W * BR_time_ratio[i][0]), round(H * BR_time_ratio[i][1])) 
-        TL_time.append(TL)
-        BR_time.append(BR)
-
-    TL_stage = [(round(W * TL_stage_ratio[0][0]), round(H * TL_stage_ratio[0][1]))]
-    BR_stage = [(round(W * BR_stage_ratio[0][0]), round(H * BR_stage_ratio[0][1]))]     
+    # 取得状況の把握
+    if (os.path.exists(out_path)):
+        print('情報取得済み')
+    else:
+        video = cv2.VideoCapture(video_path)   
+        H = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        W = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+        fps = video.get(cv2.CAP_PROP_FPS)
+        frame_all = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
         
-    # 「ル」の画像
-    img_read = cv2.imread('.\\keyobject\\rule.png')
-    img_rule = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
+        # 動画の画面サイズに応じて切り取り範囲を設定
+        TL_rule = []
+        BR_rule = []
+        for i in range(len(TL_rule_ratio)):
+            TL = (round(W * TL_rule_ratio[i][0]), round(H * TL_rule_ratio[i][1]))
+            BR = (round(W * BR_rule_ratio[i][0]), round(H * BR_rule_ratio[i][1])) 
+            TL_rule.append(TL)
+            BR_rule.append(BR)
     
-
-    # 試合終了判定画像
-    img_read = cv2.imread('.\\keyobject\\time_fin.png')
-    img_fin = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
-        
+        TL_time = []
+        BR_time = []
+        for i in range(len(TL_time_ratio)):
+            TL = (round(W * TL_time_ratio[i][0]), round(H * TL_time_ratio[i][1]))
+            BR = (round(W * BR_time_ratio[i][0]), round(H * BR_time_ratio[i][1])) 
+            TL_time.append(TL)
+            BR_time.append(BR)
     
-    # ここから動画処理
-    frame_rule  = 0
-    rule = 'nodata'
-    stage_num = 0
-    frame_start = 0
-    frame_end   = 0
-    
-    val_list = []
-    fcount = 0
-    while(video.isOpened()):
-        ret, frame = video.read()
-    
-        if not ret:
-            break
-      
-        fcount = fcount + 1  
-        
-        if frame_rule == 0:
-            ''' ルール表示の判定 '''
-            img_trm = frame[TL_rule[0][1] : BR_rule[0][1], TL_rule[0][0] : BR_rule[0][0]]
-            jug = cv2.matchTemplate(img_trm, img_rule, cv2.TM_CCORR_NORMED)
-            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
-               
-            if maxVal > threshold:     
-                frame_rule = fcount + round(fps*1)              
-        
-        elif fcount == frame_rule:
-            ''' ルールの判定 '''
-            img_trm = frame[TL_rule[1][1] : BR_rule[1][1], TL_rule[1][0] : BR_rule[1][0]]            
+        TL_stage = [(round(W * TL_stage_ratio[0][0]), round(H * TL_stage_ratio[0][1]))]
+        BR_stage = [(round(W * BR_stage_ratio[0][0]), round(H * BR_stage_ratio[0][1]))]     
             
-            val_rule = 0 
-            for rule_name in rule_list:
-                img_read = cv2.imread('.\\keyobject\\rule_' + rule_name + '.png')
-                img_rname = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920) 
-                
-                jug = cv2.matchTemplate(img_trm, img_rname, cv2.TM_CCORR_NORMED)
-                minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
-                
-                if maxVal > val_rule:
-                    rule = rule_name
-                    val_rule = maxVal
-                              
-            ''' ステージの判定 '''
-            img_trm = frame[TL_stage[0][1] : BR_stage[0][1], TL_stage[0][0] : BR_stage[0][0]]  
-            
-            val_stage = 0 
-            for i in range(0, 23):
-                img_read = cv2.imread('.\\keyobject\\stage_name_' + str(i) + '.png')
-                img_stage = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
-                
-                jug = cv2.matchTemplate(img_trm, img_stage, cv2.TM_CCORR_NORMED)
-                minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug)
-                
-                if maxVal > val_stage:
-                    stage_num = i
-                    val_stage = maxVal       
-                    
-            ''' 試合開始判定画像を読み込む '''
-            # ルールに応じて変える
-            if rule == 'turf':
-                img_read = cv2.imread('.\\keyobject\\time_259.png')
-                img_time = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
-                
-            else:
-                img_read = cv2.imread('.\\keyobject\\time_459.png')
-                img_time = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920) 
-                   
+        # 「ル」の画像
+        img_read = cv2.imread('.\\keyobject\\rule.png')
+        img_rule = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
+        
     
-        elif fcount > frame_rule:
-            if frame_start == 0:               
-                ''' 試合開始の判定 '''
-                img_trm = frame[TL_time[0][1] : BR_time[0][1], TL_time[0][0] : BR_time[0][0]]
-                jug = cv2.matchTemplate(img_trm, img_time, cv2.TM_CCORR_NORMED)
-                minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
-                   
-                if maxVal > threshold:     
-                    frame_start = fcount - round(fps*1)  
-
+        # 試合終了判定画像
+        img_read = cv2.imread('.\\keyobject\\time_fin.png')
+        img_fin = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
             
-            elif frame_end == 0:
-                ''' 試合終了の判定 '''
-                img_trm = frame[TL_time[1][1] : BR_time[1][1], TL_time[1][0] : BR_time[1][0]]
-                jug = cv2.matchTemplate(img_trm, img_fin, cv2.TM_CCORR_NORMED)
-                minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
-                
-                val_list.append([fcount, maxVal])
-                   
-                if maxVal > threshold:     
-                    frame_end = fcount    
-
-            else:       
+        
+        # ここから動画処理
+        frame_rule  = 0
+        rule = 'nodata'
+        stage_num = 0
+        frame_start = 0
+        frame_end   = 0
+        
+        val_list = []
+        fcount = 0
+        while(video.isOpened()):
+            ret, frame = video.read()
+        
+            if not ret:
                 break
-                                                  
-    video.release
+          
+            fcount = fcount + 1  
+            
+            if frame_rule == 0:
+                ''' ルール表示の判定 '''
+                img_trm = frame[TL_rule[0][1] : BR_rule[0][1], TL_rule[0][0] : BR_rule[0][0]]
+                jug = cv2.matchTemplate(img_trm, img_rule, cv2.TM_CCORR_NORMED)
+                minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
+                   
+                if maxVal > threshold:     
+                    frame_rule = fcount + round(fps*1)              
+            
+            elif fcount == frame_rule:
+                ''' ルールの判定 '''
+                img_trm = frame[TL_rule[1][1] : BR_rule[1][1], TL_rule[1][0] : BR_rule[1][0]]            
+                
+                val_rule = 0 
+                for rule_name in rule_list:
+                    img_read = cv2.imread('.\\keyobject\\rule_' + rule_name + '.png')
+                    img_rname = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920) 
+                    
+                    jug = cv2.matchTemplate(img_trm, img_rname, cv2.TM_CCORR_NORMED)
+                    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
+                    
+                    if maxVal > val_rule:
+                        rule = rule_name
+                        val_rule = maxVal
+                                  
+                ''' ステージの判定 '''
+                img_trm = frame[TL_stage[0][1] : BR_stage[0][1], TL_stage[0][0] : BR_stage[0][0]]  
+                
+                val_stage = 0 
+                for i in range(0, 23):
+                    img_read = cv2.imread('.\\keyobject\\stage_name_' + str(i) + '.png')
+                    img_stage = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
+                    
+                    jug = cv2.matchTemplate(img_trm, img_stage, cv2.TM_CCORR_NORMED)
+                    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug)
+                    
+                    if maxVal > val_stage:
+                        stage_num = i
+                        val_stage = maxVal       
+                        
+                ''' 試合開始判定画像を読み込む '''
+                # ルールに応じて変える
+                if rule == 'turf':
+                    img_read = cv2.imread('.\\keyobject\\time_259.png')
+                    img_time = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920)
+                    
+                else:
+                    img_read = cv2.imread('.\\keyobject\\time_459.png')
+                    img_time = cv2.resize(img_read, dsize=None, fx=H/1080, fy=W/1920) 
+                       
+        
+            elif fcount > frame_rule:
+                if frame_start == 0:               
+                    ''' 試合開始の判定 '''
+                    img_trm = frame[TL_time[0][1] : BR_time[0][1], TL_time[0][0] : BR_time[0][0]]
+                    jug = cv2.matchTemplate(img_trm, img_time, cv2.TM_CCORR_NORMED)
+                    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
+                       
+                    if maxVal > threshold:     
+                        frame_start = fcount - round(fps*1)  
     
+                
+                elif frame_end == 0:
+                    ''' 試合終了の判定 '''
+                    img_trm = frame[TL_time[1][1] : BR_time[1][1], TL_time[1][0] : BR_time[1][0]]
+                    jug = cv2.matchTemplate(img_trm, img_fin, cv2.TM_CCORR_NORMED)
+                    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug) 
+                    
+                    val_list.append([fcount, maxVal])
+                       
+                    if maxVal > threshold:     
+                        frame_end = fcount    
     
-    status_list = [['rule', 'stage_num', 'fps', 'frame_all', 'frame_start', 'frame_end'],
-                   [rule, stage_num, fps, frame_all, frame_start, frame_end]]
+                else:       
+                    break
+                                                      
+        video.release
+
+        if rule == 'zones':
+            idx = stage_num + 1
+            zones_num = stage_list[idx][2]
+        else:
+            zones_num = 0
+        
+        
+        status_list = [['rule', 'stage_num', 'zones_num', 'fps', 'frame_all', 'frame_start', 'frame_end'],
+                       [rule, stage_num, zones_num, fps, frame_all, frame_start, frame_end]] 
+        
+        # CSV出力
+        with open(out_path, 'w') as file:
+            writer = csv.writer(file, lineterminator='\n')
+            writer.writerows(status_list)  
+        
+        print('完了')
+        
     
-    # CSV出力
-    with open(out_path, 'w') as file:
-        writer = csv.writer(file, lineterminator='\n')
-        writer.writerows(status_list)  
-    
-    
-    
+
 
 def main():
-    ''' メイン処理 '''
-    match_list = [[3, '3-1'],
-                  [3, '3-2'],
-                  [3, '3-3'],
-                  [3, '2-2'],
-                  [2, '1-3'],
-                  [2, '2-2']]
+    ''' メイン処理 '''   
+    for i in range(2, 6):
+        # ビデオディレクトリ取得 '*'はワイルドカード
+        fnames = glob.glob('.\\DAY' + str(i) + '\\PL-DAY' + str(i) + '_*.avi' )
+        
+        for video_path in fnames:
+            video_name, video_ext = os.path.splitext(os.path.basename(video_path))
+            video_dir = os.path.dirname(video_path)    
+          
+            out_path = video_dir + '\\' + video_name + '_status.csv'
+            
+            print('=========================')
+            print(video_name)
 
-    
-    for day, match in match_list:
-        video_path = '.\\DAY' + str(day) + '\\PL-DAY' + str(day) + '_' + match + '.avi'  
-        
-        video_name, video_ext = os.path.splitext(os.path.basename(video_path))
-        video_dir = os.path.dirname(video_path)    
-      
-        out_path = video_dir + '\\' + video_name + '_status.csv'
-    
-        
-        getStatus(video_path, out_path)
-    
+            getStatus(video_path, out_path)
+            
+            
+        print('=========================')
+
 
     
 if __name__ == "__main__":    
