12af82de4bd696186c35684f19975ba891b67c6a
MasafumiIwanaga <nfk.atomic.kyoro.3.5@gmail.com>
2020-07-06 05:27:04 +0900

    ナワバリで全体の色割合を取得

--- ikaColor.py
+++ ikaColor.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
-# coding: utf-8
-# In[ ]:
-# -*- coding: utf-8 -*-
+
 import cv2
 import csv
 import os.path
 import numpy as np
 import pandas as pd
 
-# 指定した位置(x,y)の半径rの範囲の円内の色割合を取得する関数
-# def getColorRatio(base_frame, frame, x, y, r):
-    # 初期画像との差分で比較する
-        #hsv = cv2.absdiff(base_hsv, hsv)
 
 def getColorRatio(frame, x, y, r):
-    # フレームをHSVに変換
+        # フレームをHSVに変換
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = np.zeros(hsv.shape, dtype=np.uint8)
         cv2.circle(mask, (x, y), r, (255, 255, 255), -1)
         hsv = hsv & mask
-        #hsv = hsv[top : bottom, left : right]
+        # hsv = hsv[top : bottom, left : right]
         # 取得する色の範囲を指定する
         lower_yellow = np.array([20, 100, 140])
         upper_yellow = np.array([90, 255, 255])
@@ -35,10 +29,33 @@ def getColorRatio(frame, x, y, r):
         blue_ratio = cv2.countNonZero(binary_blue) / (r * r * 3.14)
         other_ratio =  1 - yellow_ratio - blue_ratio
 
-        # numpyで配列を作って返す
-        arr = np.asarray([yellow_ratio, blue_ratio, other_ratio])
+        # リストを作って返す
+        list = [yellow_ratio, blue_ratio, other_ratio]
         return arr
 
+
+def getColorRatioNawabari(frame):
+        # フレームをHSVに変換
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # 取得する色の範囲を指定する
+        lower_yellow = np.array([20, 100, 140])
+        upper_yellow = np.array([90, 255, 255])
+               
+        lower_blue = np.array([110, 100, 90])
+        upper_blue = np.array([254, 255, 255])
+               
+        # HSV の範囲指定で2値化する。
+        binary_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        binary_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+        # 各色の割合
+        yellow_count = cv2.countNonZero(binary_yellow)
+        blue_count = cv2.countNonZero(binary_blue) 
+
+        # リストを作って返す
+        list = [yellow_count, blue_count]
+        return list
 
 def trackingColor(video_path, frame_start, frame_end):
    
@@ -62,16 +79,7 @@ def trackingColor(video_path, frame_start, frame_end):
                        # 色割合をリストに格納
                        color_ratio = getColorRatio(frame, 540, 960, 1920)
                        color_arr.append(color_ratio)
-                       '''
-                       # 指定した色に基づいたマスク画像の生成
-                       #img_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-                       img_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-                       # フレーム画像とマスク画像の共通の領域を抽出する。
-                       img_color = cv2.bitwise_and(frame, frame, mask=img_mask)
-                       cv2.imshow("SHOW COLOR IMAGE", img_color)
-           
-                       out.write(img_color)
-                       '''
+                      
                        # qを押したら終了
                        k = cv2.waitKey(1)
                        if k == ord('q'):
@@ -83,9 +91,11 @@ def trackingColor(video_path, frame_start, frame_end):
         #out.release
         # CSV出力
         np.savetxt(video_name + '_tracking_color_ratio.csv', color_arr, delimiter=',')
+
 def main():
         video_path = 'overlook_turf_stage0.mp4'
         trackingColor(video_path, 484, 5887)   
+
 if __name__ == "__main__":
         main()
 
---
+++ ikaTurf.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import csv
+import os.path
+
+import ikaColor
+
+# 対象動画情報(MatchStatus等から取得)
+match = 'PL-DAY1_1-1'
+frame_start = 400
+frame_end = 6200
+
+# 何フレームごとに処理を行うか
+frame_skip = 30
+
+#あとみーのディレクトリ
+video_path = 'C:\\Users\\nfkat\\Documents\\splatoon\\PremiereLeague\\' + match + '.mp4'
+#video_path = 'D:\splatoon_movie\PremiereLeague\DAY2\\' + match + '.avi'
+video_name, video_ext = os.path.splitext(os.path.basename(video_path))
+
+csv_path = video_name + '_turf_color.csv'
+
+
+def getColorRatioTurf():
+    ''' 動画処理 '''
+    video = cv2.VideoCapture(video_path)
+    W = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    H = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    
+    # 以下フレーム処理結果の準備
+    # 記録用のリスト
+    list_top = []
+
+    # 色ピクセル数
+    list_top.append('yellow_count')
+    list_top.append('blue_count')
+
+    # 各フレームでの黄色と青の合計ピクセル数
+    list_top.append('sum_count')
+
+    # 各フレームでの塗り残しを考慮しない色割合
+    list_top.append('yellow_ratio1')
+    list_top.append('bule_ratio1')
+
+    # 各フレームでの塗り残しを考慮した色割合
+    list_top.append('yellow_ratio2')
+    list_top.append('blue_ratio2')
+    list_top.append('other_ratio')
+     
+    record = [list_top]
+
+    max_color_count = 0
+           
+    # 動画処理
+    fcount = 0
+    while(video.isOpened()):
+        ret, frame = video.read()
+    
+        if not ret:
+            break
+      
+        fcount += 1  
+            
+        if frame_start <= fcount < frame_end:
+            if (fcount- frame_start) % frame_skip == 0:              
+                # フレームに対しての処理
+               
+                # 記録用のリスト
+                record_frame = []
+                
+                color = ikaColor.getColorRatioNawabari(frame)
+                record_frame.append(color[0])
+                record_frame.append(color[1])
+                record_frame.append(color[0] + color[1])
+                record_frame.append(color[0] /(color[0] + color[1] + 1) * 100)
+                record_frame.append(100 - record_frame[3])
+                
+                record.append(record_frame)
+
+                if(fcount <= frame_end -1):
+                    max_color_count = record_frame[2]
+  
+        if fcount == frame_end:
+           break
+               
+    video.release
+
+    print(max_color_count) 
+   
+    for i in range(len(record)):
+        if(i > 0):
+            record[i].append(record[i][0] / max_color_count * 100)
+            record[i].append(record[i][1] / max_color_count * 100)
+            record[i].append(100 - record[i][5] - record[i][6])
+    
+
+    # CSV出力
+    with open(csv_path, 'w') as file:
+        writer = csv.writer(file, lineterminator='\n')
+        writer.writerows(record)
+
+def main():
+    ''' メイン処理 '''
+    getColorRatioTurf()
+    
+        
+if __name__ == "__main__":
+    main()
