0169da24910838b4a512bfb0800502e3058eef53
tonightyamada99 <tonightyamada99@gmail.com>
2021-02-19 23:53:45 +0900

    イカランプ関連はほぼ完成

--- python/ikaLamp.py
+++ python/ikaLamp.py
@@ -47,13 +47,6 @@ C_lamp = {'SS':[607, 683, 758, 834, 1086, 1162, 1237, 1313],
 # 幅（半分）
 W_lamp = {'SS':35, 'SM':37, 'MM':39, 'ML':41, 'LL':43}
 
-# 中立状態（将来的に削除）
-L_lamp_n = [518, 629, 709, 792, 1047, 1130, 1220, 1311]
-R_lamp_n = [608, 699, 789, 872, 1127, 1210, 1290, 1401]
-# 主観視点でのイカランプ上端座標
-thd_ika = [17, 19, 21, 23, 26]
-thd_oct = []
-
 # マップでのプレイヤー表示位置
 # 味方
 TL_map = [(125, 490), ( 830,  40), (1535, 490)]
@@ -71,7 +64,6 @@ size_button = [25, 27, 28.5, 30.5, 32]
 # 面積閾値
 thd_button = 300   # @FHD(1920, 1080)
 
-# 閾値 threshold
 # 一致率
 thd_val = 0.9
 
@@ -101,6 +93,21 @@ bin_bgs = cv2.imread('.\\pbm\\map_bg_spw.pbm', -1)
 bin_crs  = [bin_ggc, bin_bgc]
 bin_spw  = [bin_ggs, bin_bgs]
 
+# 各サイズのランプ画像をリスト化
+ika_list = []
+oct_list = []
+cross_list = []
+for size in size_list:
+    # イカ
+    bin_tmp = cv2.imread('pbm\\lamp_ika_' + size + '.pbm', -1)
+    ika_list.append(bin_tmp)
+    # タコ
+    bin_tmp = cv2.imread('pbm\\lamp_oct_' + size + '.pbm', -1)
+    oct_list.append(bin_tmp)
+    # バツ印
+    bin_tmp = cv2.imread('pbm\\lamp_cross_' + size + '.pbm', -1)
+    cross_list.append(bin_tmp)
+
 
 def listtop():
     ''' 記録リストの先頭行を返す '''
@@ -109,22 +116,22 @@ def listtop():
     return list_top
 
 
-def frameObjective(frame, team_color):
-    ''' 俯瞰視点:フレームに対しての一連の処理を行う '''
+def forFrameObjective(frame, team_color):
+    ''' 俯瞰視点：フレームに対しての一連の処理を行う '''
     # ランプの大きさ
     lamp_size = getSizeObjective(frame)
     # 生存状況
-    lamp_list = getStatus(frame, team_color, 'obj', lamp_size)
+    lamp_list = getStatusObjective(frame, team_color, lamp_size)
 
     return lamp_list
 
 
-def frameSubjective(frame, team_color):
-    ''' 主観視点:フレームに対しての一連の処理を行う '''
+def forFrameSubjective(frame, team_color, shape_list):
+    ''' 主観視点：フレームに対しての一連の処理を行う '''
     # ランプの大きさ
-    lamp_size = getSizeSubjective(frame)
+    lamp_size = getSizeSubjective(frame, team_color, shape_list)
     # 生存状況
-    lamp_list = getStatus(frame, team_color, 'sub', lamp_size)
+    lamp_list = getStatusSubjective(frame, team_color, shape_list, lamp_size)
 
     return lamp_list
 
@@ -171,12 +178,12 @@ def getShape(frame, team_color):
         idx = 0 if i<=3 else 1
         thd = team_color[idx]
         # イカランプ座標（中心はMM、大きさはLLの範囲）
-        l = C_lamp['MM'][i] - W_lamp['LL']
-        r = C_lamp['MM'][i] + W_lamp['LL']
-        t = T_lamp['subLL']
-        b = B_lamp['subLL']
-        TL = (l, t)
-        BR = (r, b)
+        left   = C_lamp['MM'][i] - W_lamp['LL']
+        right  = C_lamp['MM'][i] + W_lamp['LL']
+        top    = T_lamp['subLL']
+        bottom = B_lamp['subLL']
+        TL = (left, top)
+        BR = (right, bottom)
         # 一致率が大きい比較画像のインデックスを取得
         index = iip.getMostMatchImage(frame, TL, BR, tmp_list,
                                       threshold=thd, color_type='HSV')
@@ -188,97 +195,8 @@ def getShape(frame, team_color):
     return shape_list
 
 
-def readLampImage():
-    ''' 主観視点の状態把握に使う画像読み込み '''
-    # 各サイズ画像をリストで出力
-    ika_list = []
-    oct_list = []
-    cross_list = []
-    for size in size_list:
-        # イカ
-        bin_tmp = cv2.imread('pbm\\lamp_ika_' + size + '.pbm', -1)
-        ika_list.append(bin_tmp)
-        # タコ
-        bin_tmp = cv2.imread('pbm\\lamp_oct_' + size + '.pbm', -1)
-        oct_list.append(bin_tmp)
-        # バツ印
-        bin_tmp = cv2.imread('pbm\\lamp_cross_' + size + '.pbm', -1)
-        cross_list.append(bin_tmp)
-
-    return ika_list, oct_list, cross_list
-
-
-def getSizeSubjective(frame, team_color, lamp_list, cross_list):
-    '''
-    主観：ランプの大きさを取得する
-    左から4番目を基準イカランプとしてその大きさを調べる
-    '''
-    # ランプ大きさ SS, SM, MM, ML, LL
-    lamp_size = ['nodata', 'nodata']
-
-    # 基準イカランプ切り出し
-    top    = T_lamp['subLL']
-    bottom = B_lamp['subLL']
-    left   = L_lamp['LL'][3]
-    right  = R_lamp['LL'][3]
-    img_trm = frame[top:bottom, left:right]
-    # BGR -> HSV
-    hsv_trm = cv2.cvtColor(img_trm, cv2.COLOR_BGR2HSV)
-
-    # 基準イカランプの状態を把握する
-    # 基準色
-    color_list = ['blk', team_color[0], 'spw']
-    # マスク処理でランプ外の部分の影響を小さくする
-    msk_tmp = lamp_list[4]
-    # 抽出された面積が最も大きい色を探す
-    s_max = 0
-    color_max = 'nodata'
-    bin_max = 0
-    for color in color_list:
-        # 閾値を取得
-        thd_min, thd_max = thd_hsv[color]
-        # 基準色で2値化
-        bin_trm = cv2.inRange(hsv_trm, thd_min, thd_max)
-        # マスク処理
-        msk_trm = cv2.bitwise_and(bin_trm, msk_tmp)
-        # 抽出された部分の面積を取得
-        s = cv2.countNonZero(msk_trm)
-
-        if s > s_max:
-            s_max = s
-            color_max = color
-            bin_max = bin_trm
-
-    # 黒以外の場合は状態取得で使った二値化画像を流用
-    if color_max != 'blk':
-        tmp_list = lamp_list
-        bin_tgt = bin_max
-    # 黒の場合はバツ印で大きさ判別するためグレーで二値化
-    else:
-        tmp_list = cross_list
-        bin_tgt = cv2.inRange(img_trm, thd_rgb['gry'][0], thd_rgb['gry'][1])
-
-    # 候補画像で一致率が最も高いものを探す
-    val_max = 0
-    index = 'nodata'
-    for i, template in enumerate(tmp_list):
-        # 一致率算出
-        jug = cv2.matchTemplate(bin_tgt, template, cv2.TM_CCORR_NORMED)
-        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug)
-
-        if maxVal > val_max:
-            val_max = maxVal
-            index = i
-
-    # 大きさのインデックス == サイズ名インデックス
-    lamp_size[0] = size_list[index]
-    lamp_size[1] = size_list[4-index]
-
-    return lamp_size
-
-
 def getSizeObjective(frame):
-    ''' 俯瞰：ランプの大きさを取得する '''
+    ''' 俯瞰視点：ランプの大きさを取得する '''
     # ランプ大きさ SS, SM, MM, ML, LL
     lamp_size = ['nodata', 'nodata']
 
@@ -311,8 +229,76 @@ def getSizeObjective(frame):
     return lamp_size
 
 
-def getStatus(frame, team_color, viewpoint, lamp_size):
-    ''' イカランプの状態を取得する '''
+def getSizeSubjective(frame, team_color, shape_list):
+    '''
+    主観視点：ランプの大きさを取得する
+    左から4番目を基準イカランプとしてその大きさを調べる
+    '''
+    # ランプ大きさ SS, SM, MM, ML, LL
+    lamp_size = ['nodata', 'nodata']
+
+    # ランプの形の画像
+    tmp_list = ika_list if shape_list[3] == 'ika' else oct_list
+    # 基準イカランプ切り出し
+    top    = T_lamp['subLL']
+    bottom = B_lamp['subLL']
+    left   = L_lamp['LL'][3]
+    right  = R_lamp['LL'][3]
+    img_trm = frame[top:bottom, left:right]
+    # BGR -> HSV
+    hsv_trm = cv2.cvtColor(img_trm, cv2.COLOR_BGR2HSV)
+
+    # 基準イカランプの状態を把握する
+    # 基準色
+    color_list = ['blk', team_color[0], 'spw']
+    # LLの画像でマスクしてランプ外の影響を減らす
+    bin_shp = tmp_list[4]
+    # 抽出された面積が最も大きい色を探す
+    s_max = 0
+    color_max = 'nodata'
+    bin_max = 0
+    for color in color_list:
+        # 閾値を取得
+        thd_min, thd_max = thd_hsv[color]
+        # 基準色で2値化
+        bin_trm = cv2.inRange(hsv_trm, thd_min, thd_max)
+        # マスク処理
+        msk_trm = cv2.bitwise_and(bin_trm, bin_shp)
+        # 抽出された部分の面積を取得
+        s = cv2.countNonZero(msk_trm)
+
+        if s > s_max:
+            s_max = s
+            color_max = color
+            bin_max = bin_trm
+
+    # 黒以外の場合は状態取得で使った二値化画像を流用
+    # 黒の場合はバツ印で大きさ判別するためグレーで二値化
+    if color_max == 'blk':
+        tmp_list = cross_list
+        bin_max = cv2.inRange(img_trm, thd_rgb['gry'][0], thd_rgb['gry'][1])
+
+    # 候補画像で一致率が最も高いものを探す
+    val_max = 0
+    index = 2
+    for i, template in enumerate(tmp_list):
+        # 一致率算出
+        jug = cv2.matchTemplate(bin_max, template, cv2.TM_CCORR_NORMED)
+        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(jug)
+
+        if maxVal > val_max:
+            val_max = maxVal
+            index = i
+
+    # 大きさのインデックス == サイズ名インデックス
+    lamp_size[0] = size_list[index]
+    lamp_size[1] = size_list[4-index]
+
+    return lamp_size
+
+
+def getStatusObjective(frame, team_color, lamp_size):
+    ''' 俯瞰視点：イカランプの状態を取得する '''
     # 記録リスト 0:dead  1:alive  2:sp
     lamp_list = ['nodata' for i in range(8)]
 
@@ -326,12 +312,47 @@ def getStatus(frame, team_color, viewpoint, lamp_size):
         # イカランプ座標
         left   = L_lamp[lamp_size[idx]][i]
         right  = R_lamp[lamp_size[idx]][i]
-        top    = T_lamp[viewpoint + lamp_size[idx]]
-        bottom = B_lamp[viewpoint + lamp_size[idx]]
+        top    = T_lamp['obj' + lamp_size[idx]]
+        bottom = B_lamp['obj' + lamp_size[idx]]
         TL = (left, top)
         BR = (right, bottom)
         # 最も面積を占める色を取得
         max_color = iip.getMostLargeColor(frame, TL, BR, lamp_color[idx])
+        # 最大の色のインデックス -> イカランプの状態
+        status = lamp_color[idx].index(max_color)
+        # リストに記録
+        lamp_list[i] = status
+
+    return lamp_list
+
+
+def getStatusSubjective(frame, team_color, shape_list, lamp_size):
+    ''' 主観視点：イカランプの状態を取得する '''
+    # 記録リスト 0:dead  1:alive  2:sp
+    lamp_list = ['nodata' for i in range(8)]
+
+    # 基準色リスト [黒, チームカラー, スペシャル状態]
+    lamp_color = [['blk', color, 'spw'] for color in team_color]
+
+    # 8プレイヤー分
+    for i in range(8):
+        # サイズと色のインデックスはiが3以下は0、4以上は1
+        idx = 0 if i<=3 else 1
+        size = lamp_size[idx]
+        thd = lamp_color[idx]
+        # イカランプ座標
+        left   = C_lamp[size][i] - W_lamp['LL']
+        right  = C_lamp[size][i] + W_lamp['LL']
+        top    = T_lamp['subLL']
+        bottom = B_lamp['subLL']
+        TL = (left, top)
+        BR = (right, bottom)
+        # イカランプの形の画像
+        shape = shape_list[i]
+        index = size_list.index(size)
+        bin_shp = ika_list[index] if shape == 'ika' else oct_list[index]
+        # 最も面積を占める色を取得
+        max_color = iip.getMostLargeColor(frame, TL, BR, thd, mask=bin_shp)
         # 最大の色のインデックス -> イカランプの状態
         status = lamp_color[idx].index(max_color)
         # リストに記録
@@ -384,7 +405,7 @@ def test():
 
     viewpoint = 'sub'
     rule = 'zones'
-    target_name = 'image_' + viewpoint + '_' + rule + '_02.png'
+    target_name = 'image_' + viewpoint + '_' + rule + '_05.png'
 
     img_dir = 'capture_image\\'
     target_path = img_dir + target_name
@@ -403,7 +424,7 @@ def test():
             lamp_size = getSizeObjective(frame)
             print('lamp_size ', lamp_size)
 
-            lamp_list = getStatus(frame, team_color, viewpoint, lamp_size)
+            lamp_list = getStatusSubjective(frame, team_color, lamp_size)
             print('lamp_list ', lamp_list)
 
         # 主観視点
@@ -411,15 +432,19 @@ def test():
             team_color = getTeamColor(frame)
             print('team_color', team_color)
 
-            shape_list = getShape(frame, team_color)
-            print(shape_list[0:4])
-            print(shape_list[4:8])
+            # shape_list = getShape(frame, team_color)
+            # print(shape_list[0:4])
+            # print(shape_list[4:8])
 
-            # danger_num = judgeDanger(frame)
-            # print('danger_num', danger_num)
+            shape_list = ['ika', 'oct', 'ika', 'ika',
+                          'ika', 'oct', 'oct', 'oct']
 
-            # lamp_list = getStatus(frame, team_color, viewpoint, lamp_size)
-            # print('lamp_list ', lamp_list)
+
+            lamp_size = getSizeSubjective(frame, team_color, shape_list)
+            print('lamp_size ', lamp_size)
+
+            status_list = getStatusSubjective(frame, team_color, shape_list, lamp_size)
+            print('status    ', status_list)
 
         # マップ画面
         else:
