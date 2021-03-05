9fa02b587d47afa6e7dcad033c3e9fdf406b3ea5
tonightyamada99 <tonightyamada99@gmail.com>
2021-03-05 23:44:51 +0900

    取得したデータを補正する（いろいろ改善が必要）

--- python/ikaCorrectData.py
+++ python/ikaCorrectData.py
@@ -8,139 +8,162 @@ import numpy as np
 import csvread
 
 
-# 試合開始からの時間
+# 試合開始からのフレーム数
 length_start = 30
-
-
-def correctCount(count_list):
-    ''' カウントを補正する '''
-    # 試合開始直後の補正
-    count_list[1:length_start+1] = [100 for i in range(length_start)]
-
-    for i in range(2, len(count_list)):
-        # 現在と1つ前の値
-        count_pre = count_list[i-1]
-        count_now = count_list[i]
-        # データ無し -> -1に置換
-        if count_now == 'nodata':
-            cor_count = -1
-        # データ有り & 前フレームがデータ無し -> そのまま記録
-        elif count_pre == -1:
-            cor_count = count_now
-        # 両方データ有り -> 差分を算出
-        else:
-            dif = float(count_pre) - float(count_now)     # 差分 diffrence
-            # カウントは前のフレームと変わらないか1減るかのどちらか
-            if dif == 0 or dif == 1:
-                cor_count = count_now
-            else:
-                cor_count = count_pre
-
-        # リストを置換
-        count_list[i] = cor_count
-
-    return count_list
-
-
-def correctPenaltyCount(count_list):
-    ''' ペナルティカウントを補正する '''
-    # 試合開始直後の補正
-    count_list[1:length_start+1] = [0 for i in range(length_start)]
-
-    # 誤認識とデータ無しの補正
-    for i in range(2, len(count_list)):
-        # 現在と1つ前の値
-        count_pre = count_list[i-1]
-        count_now = count_list[i]
-        # データ無し -> -1に置換
-        if count_now == 'nodata':
-            cor_count = -1
-        # データ有り & 1つ前がデータ無し -> そのまま記録
-        elif count_pre == -1:
-            cor_count = count_now
-        # 両方データ有り -> 差分を算出
-        else:
-            dif = float(count_pre) - float(count_now)     # 差分 diffrence
-            # カウントは前のフレームと変わらないか1減るか
-            # 増えた場合は新たにペナルティが追加されたと判断
-            if dif <= 1:
-                cor_count = count_now
-            # １以上減っている場合は前の値を記録
-            else:
-                cor_count = count_pre
-
-        # リストを置換
-        count_list[i] = cor_count
-
-    return count_list
+# 試合終了までのフレーム数
+length_end = 6
+# データ無しを示す文字列
+str_none = 'nodata'
+# データ無しの置換先 replace
+rep_none = -2
 
 
 def correct(video_path):
-    ''' カウントの修正 '''
+    ''' データを補正する '''
+    # 動画の名前の取得
     video_name, video_ext = os.path.splitext(os.path.basename(video_path))
     video_dir = os.path.dirname(video_path)
-    # 入力パス
+    # 入力元
     info_path = video_dir + '\\' + video_name + '_info.csv'
-    data_path = video_dir + '\\' + video_name + '_count.csv'
-    # 出力パス
-    out_path  = video_dir + '\\' + video_name + '_count_correct.csv'
+    status_path = video_dir + '\\' + video_name + '_status.csv'
+    data_path   = video_dir + '\\' + video_name + '_count.csv'
+    # 出力先
+    out_status_path = video_dir + '\\' + video_name + '_correct_status.csv'
+    out_count_path  = video_dir + '\\' + video_name + '_correct_count.csv'
 
-    if not os.path.exists(info_path):
+    if not os.path.exists(status_path) or not os.path.exists(data_path):
         print('動画のカウントファイルが見つかりません。')
-
-    elif not os.path.exists(data_path):
-        print('動画のカウントファイルが見つかりません。')
-
     else:
+        # 動画の情報を読み込む
+        info_dict = csvread.readAsDict(info_path)
+        rule = info_dict['rule']
+
         # カウントファイルを読み込む
         data_list = csvread.readAsList(data_path)
         data_array = np.array(data_list)
-        array_top = data_array[0]
-
-        # 出力リスト
-        fcount_array  = data_array[:, 0]
-        correct_array = np.array([fcount_array]).T
 
         # カウントの補正
-        # 対象列の位置を取得
-        idx_list = [i for i in range(len(array_top)) \
-                    if array_top[i].startswith('count')]
-        for index in idx_list:
-            # 対象列の抜粋
-            cor_count_array = data_array[:, index]
-            # 補正処理
-            cor_count_array = correctCount(cor_count_array)
-            # リストに追加
-            add_array = np.array([cor_count_array]).T
-            correct_array = np.append(correct_array, add_array, axis=1)
-
+        data_array = correctCount(data_array)
         # ペナルティカウントの補正
-        idx_list = [i for i in range(len(array_top)) \
-                    if array_top[i].startswith('penalty')]
-        for index in idx_list:
-            # 対象列の抜粋
-            cor_count_array = data_array[:, index]
-            # 補正処理
-            cor_count_array = correctPenaltyCount(cor_count_array)
-            # リストに追加
-            add_array = np.array([cor_count_array]).T
-            correct_array = np.append(correct_array, add_array, axis=1)
+        data_array = correctPenaltyCount(data_array)
+        # その他の補正
+        data_array = correctOthers(data_array)
+        # 試合終了直前の補正
+        data_array = correctLast(data_array)
+        # データ無しの補正
+        data_array = np.where(data_array==str_none, rep_none, data_array)
 
         # CSV出力
-        with open(out_path, 'w') as file:
+        with open(out_count_path, 'w') as file:
             writer = csv.writer(file, lineterminator='\n')
-            writer.writerows(correct_array)
+            writer.writerows(data_array)
+
+
+def searchListTop(list_top, target):
+    ''' 検索語から始まる要素のインデックスを返す '''
+    # インデックスリスト
+    idx_list = []
+    # 対象の型によって分岐
+    length = len(list_top)
+    if type(target) is str:
+        idx_list = [i for i in range(length) if list_top[i].startswith(target)]
+    elif type(target) is list:
+        for t in target:
+            idx_list += [i for i in range(length) if list_top[i].startswith(t)]
+
+    return idx_list
+
+
+def correctOthers(data_array):
+    ''' 確保状況・塗り状況・オブジェクト位置・アサリ数を補正する '''
+    # 対象列の名前
+    target = ['control', 'ratio', 'location', 'clam']
+    # 対象列の位置を取得
+    idx_list = searchListTop(data_array[0], target)
+
+    for index in idx_list:
+        # 試合開始直後の補正 0で置き換える
+        data_array[1:length_start+1, index] = np.zeros((1, length_start))
+
+    return data_array
+
+
+def correctCount(data_array):
+    ''' カウントを補正する '''
+    # 対象列の位置を取得
+    idx_list = searchListTop(data_array[0], 'count')
+
+    for index in idx_list:
+        # 試合開始直後の補正 100で置き換える
+        data_array[1:length_start+1, index] = np.ones((1, length_start)) * 100
+
+        # 誤認識の補正
+        for i in range(2, len(data_array)):
+            # 現在と1つ前の値
+            count_pre = data_array[i-1][index]
+            count_now = data_array[i][index]
+            if count_now != str_none and count_pre != str_none:
+                # 差分を算出 diffrence
+                dif = float(count_pre) - float(count_now)
+                # カウントは1つ前と変わらないか1減るかのどちらか
+                if dif != 0 and dif != 1:
+                    # 現在の値を1つ前の値で置換
+                    data_array[i][index] = count_pre
+
+    return data_array
+
+
+def correctPenaltyCount(data_array):
+    ''' ペナルティカウントを補正する '''
+    # 対象列の位置を取得
+    idx_list = searchListTop(data_array[0], 'penalty')
+
+    for index in idx_list:
+        # 試合開始直後の補正 0で置き換える
+        data_array[1:length_start+1, index] = np.zeros((1, length_start))
+
+        # 誤認識の補正
+        for i in range(2, len(data_array)):
+            # 現在と1つ前の値
+            count_pre = data_array[i-1][index]
+            count_now = data_array[i][index]
+            if count_now != str_none and count_pre != str_none:
+                # 差分を算出 diffrence
+                dif = float(count_pre) - float(count_now)
+                # ペナルティカウントが1以上減ることはない
+                # 増えた場合はペナルティが新たに追加された
+                if dif > 1:
+                    # 現在の値を1つ前の値で置換
+                    data_array[i][index] = count_pre
+
+    return data_array
+
+
+def correctLast(data_array):
+    ''' 試合終了直前を補正する '''
+    # データがある行の位置を取得
+    index_list = np.where(data_array[:, 1]!=str_none)[0]
+    # データのある最終行のインデックス
+    index_data = index_list[-1]
+    # 最低限補正が必要となるインデックス
+    index_end = len(data_array) - length_end
+    # 比較してより長い方（インデックスが小さい方）で補正する
+    index_last = index_data if index_data < index_end else index_end
+    # 置換元
+    last_array = data_array[index_last][1:]
+    for i in range(index_last, len(data_array)):
+         data_array[i][1:] = last_array
+
+    return data_array
 
 
 def main():
     ''' メイン処理 '''
 
-    fnames = glob.glob('D:\\splatoon_movie\\capture\\video_sub_zones_1.avi' )
+    fnames = glob.glob('D:\\splatoon_movie\\capture\\video_sub_*.mp4' )
 
     for video_path in fnames:
-        print('==================================================')
         print(os.path.basename(video_path))
-
         correct(video_path)
 
 
